a71e2187bd8534d23cc19835ab210c00c16c8f24
Junior Vidotti <jrvidotti@gmail.com>
2024-02-05 22:00:05 -0400

    axios deps

--- src/index.ts
+++ src/index.ts
@@ -92,29 +92,24 @@ export default class ChatwootClient {
 
     constructor({ config }: { config: ChatwootAPIConfig }) {
         this.chatwootAPI = config;
-
         this.client = {
             contacts: new ContactsApi({ config: config }),
             conversations: new ConversationsApi({ config: config }),
             messages: new MessagesApi({ config: config }),
         };
-
         this.platform = {
             accounts: new Accounts({ config: config }),
             accountUsers: new AccountUsers({ config: config }),
             agentBots: new AgentBots({ config: config }),
             users: new Users({ config: config }),
         };
-
         this.accountAgentBots = new AccountAgentBots({ config: config });
-
         this.agentBots = new AgentBots({ config: config });
         this.agents = new Agents({ config: config });
         this.automationRule = new AutomationRules({ config: config });
         this.cannedResponses = new CannedResponses({ config: config });
         this.contact = new Contact({ config: config });
         this.contacts = new Contacts({ config: config });
-
         this.customAttributes = new CustomAttributes({ config: config });
         this.conversationAssignment = new ConversationAssignment({ config: config });
         this.conversationLabels = new ConversationLabels({ config: config });
@@ -123,7 +118,6 @@ export default class ChatwootClient {
         this.inboxes = new Inboxes({ config: config });
         this.integrations = new Integrations({ config: config });
         this.messages = new Messages({ config: config });
-
         this.profile = new Profile({ config: config });
         this.reports = new Reports({ config: config });
         this.teams = new Teams({ config: config });
